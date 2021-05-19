bbaf97e464423e2e2b74c3e83b8f2b3ee32390ac
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-05-19 08:17:20 -0400

    refactor into class component using state

--- src/index.js
+++ src/index.js
@@ -2,17 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 
-function tick() {
-    const element = (
-        <main>
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {new Date().toLocaleTimeString()}.</h2>
-            </div>
-        </main>
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
 
-    );
-    ReactDOM.render(element, document.getElementById("root"));
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <main>
+                <div>
+                    <h1>Hello, world!</h1>
+                    <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+                </div>
+            </main>
+        );
+    }
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(<Clock />, document.getElementById("root"));
